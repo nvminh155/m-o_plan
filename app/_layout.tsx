dcdf14d36581b4b0fc816a772d6fc8183b9a68f9
nvminh155 <nvmttq@gmail.com>
2025-04-01 07:36:21 +0700

    add register notification

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,7 @@
-import { Navigator, Slot,  } from "expo-router";
+import { Navigator, Slot } from "expo-router";
 import "./global.css";
 
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -16,18 +15,13 @@ import AuthProvider from "@/contexts/AuthProvider";
 import AppProvider from "@/contexts/AppProvider";
 import { NotificationProvider } from "@/contexts/notification-context";
 
-
 import * as Notifications from "expo-notifications";
 import * as TaskManager from "expo-task-manager";
-
 
 configureReanimatedLogger({
   level: ReanimatedLogLevel.warn,
   strict: false, // Reanimated runs in strict mode by default
 });
-
-
-
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -53,8 +47,6 @@ TaskManager.defineTask(
 
 Notifications.registerTaskAsync(BACKGROUND_NOTIFICATION_TASK);
 
-
-
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
@@ -62,17 +54,17 @@ export default function RootLayout() {
     <GluestackUIProvider mode="light">
       <QueryClientProvider client={queryClient}>
         <NotificationProvider>
-        <AppProvider>
-          <AuthProvider>
-            <Navigator router={StackRouter} initialRouteName="plans">
-              <SafeAreaView className={"flex-1 scroll_smooth"}>
-                <View className="flex-1">
-                  <Slot />
-                </View>
-              </SafeAreaView>
-            </Navigator>
-          </AuthProvider>
-        </AppProvider>
+          <AppProvider>
+            <AuthProvider>
+              <Navigator router={StackRouter} initialRouteName="plans">
+                <SafeAreaView className={"flex-1 scroll_smooth"}>
+                  <View className="flex-1">
+                    <Slot />
+                  </View>
+                </SafeAreaView>
+              </Navigator>
+            </AuthProvider>
+          </AppProvider>
         </NotificationProvider>
       </QueryClientProvider>
     </GluestackUIProvider>
